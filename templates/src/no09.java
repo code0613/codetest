f87d23691626a80119c4517ff91e5558d80d603f
code0613 <vhxmfltm3000@naver.com>
2022-11-21 19:12:58 +0900

    docs: 수정 no09 no09.java 코드 수정

--- templates/src/no09.java
+++ templates/src/no09.java
@@ -4,26 +4,16 @@ class Nno09 {
 
         String front = phone_number.substring(0,phone_number.length()-4);
         String back = phone_number.substring(phone_number.length()-4);
-        front = front.replace("0","*");
-        front = front.replace("1","*");
-        front = front.replace("2","*");
-        front = front.replace("3","*");
-        front = front.replace("4","*");
-        front = front.replace("5","*");
-        front = front.replace("6","*");
-        front = front.replace("7","*");
-        front = front.replace("8","*");
-        front = front.replace("9","*");
-
+        front = front.replaceAll("[0-9]","*");
         answer = front.concat(back);
-
         return answer;
+        
     }
 }
 public class no09 {
     public static void main(String[] args) {
         String num = "01033334444";
         Nno09 nno09 = new Nno09();
-        nno09.solution(num);
+        System.out.println(nno09.solution(num));
     }
 }
