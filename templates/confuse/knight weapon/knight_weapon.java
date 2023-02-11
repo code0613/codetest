6fcdab8d36ff2db3496246238bf7f26db8395799
code0613 <vhxmfltm3000@naver.com>
2023-02-11 22:43:27 +0900

    Docs: 기사단 시간복잡도 해결

--- templates/confuse/knight weapon/knight_weapon.java
+++ templates/confuse/knight weapon/knight_weapon.java
@@ -1,30 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
+        int cnt = 0;
 
-        List<Integer> numList = new ArrayList<>();
 
-        for (int i = 1; i*i <= (int) Math.sqrt(number); i++) {
-            int cnt = 0;
-            for (int j = 1; j <= number; j++) {
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= (int) Math.sqrt(i); j++) {
                 if ((i % j) == 0){
                     cnt++;
+                    if (i != 1 && i / j != j) {
+                        cnt ++;
+                    }
                 }
             }
-            if (cnt > limit){
-                numList.add(power);
-            }else {
-                numList.add(cnt);
-            }
+            answer += cnt > limit ? power : cnt;
+            cnt=0;
         }
-
-        for (int i = 0; i < numList.size(); i++) {
-            answer += numList.get(i);
-        }
-
-        System.out.println(numList);
         System.out.println(answer);
         return answer;
     }
