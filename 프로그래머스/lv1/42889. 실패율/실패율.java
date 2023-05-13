f5453f07e8cf2222a288ee8d7bd8abcc5da1d7f8
JAE YEONG KIM <97332044+code0613@users.noreply.github.com>
2023-05-13 22:38:18 +0900

    Update 실패율.java

--- 프로그래머스/lv1/42889. 실패율/실패율.java
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -5,11 +5,9 @@ import java.util.List;
 class Solution {
     public int[] solution(int N, int[] stages) {
         double[] main = new double[N+1];
-//        double[] reach = new double[N];
         int[] answer = new int[N];
         List<double[]> doubleList = new ArrayList<>();
-
-
+        
         // 각 스테이지에 도착한 인원 산출
         for (int i = 0; i < stages.length; i++) {
             for (int j = 1; j <= N+1; j++) {
@@ -18,7 +16,6 @@ class Solution {
                 }
             }
         }
-        System.out.println(Arrays.toString(main));
 
         // 실패율 계산
         for (int i = 0; i < answer.length; i++) {
@@ -32,21 +29,13 @@ class Solution {
             } else doubleList.add(new double[]{i + 1, 0});
         }
 
-        for (double[] doubles : doubleList) {
-            System.out.println(Arrays.deepToString(new double[][]{doubles}));
-        }
+        // 내림차순 정렬
         doubleList.sort((i,j) -> Double.compare(j[1],i[1]));
 
-//        System.out.println("===================================================");
-//        for (double[] doubles : doubleList) {
-//            System.out.println(Arrays.deepToString(new double[][]{doubles}));
-//        }
         for (int i=0; i<doubleList.size(); i++) {
             answer[i] = (int)doubleList.get(i)[0];
         }
-//        System.out.println("===================================================");
 
-//        System.out.println(Arrays.toString(answer));
         return answer;
     }
 }
