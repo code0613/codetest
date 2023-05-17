43e4dd80909121d48f29723da61a770c771bce93
JAE YEONG KIM <97332044+code0613@users.noreply.github.com>
2023-05-17 20:39:42 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 2.98 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,20 +2,18 @@ class Solution {
    public String solution(int[] food) {
 
         StringBuilder left = new StringBuilder();
-        StringBuilder reverseLeft = new StringBuilder();
-
         int num = 0;
         for (int i = 1; i < food.length; i++) {
             num = food[i]/2;
 
             for (int j = 0; j < num; j++) {
                 left.append(i);
-                reverseLeft.append(i);
             }
         }
 
-        StringBuilder right = reverseLeft.reverse();
+        String answer = left + "0";
+        StringBuilder right = left.reverse();
 
-        return left + "0" + right;
+        return answer + right;
     }
 }
